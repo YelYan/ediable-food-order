dd5e3bd76d841606b51f694dfca13ce3cc16ad11
YelYan <yelyan.researchandproduction@gmail.com>
2025-10-30 01:48:14 +0630

    [add] order routes & stripe setup

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -17,7 +17,7 @@ const getRestaurant = expressAsyncHandler(async (req : Request , res : Response)
   res.status(200).json({
     success : true,
     message : "Resaturant found!",
-    restaurant,
+    data : restaurant,
   })
 })
 
---
+++ src/routes/order.route.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { jwtCheck, jwtParse } from "../middleware/auth.middleware.js";
+import orderController from "../controllers/order.controller.js";
+
+const router = express.Router();
+
+
+router.post("/checkout/create-checkout-session", jwtCheck , jwtParse , orderController.createCheckoutSession)
+
+export default router;
